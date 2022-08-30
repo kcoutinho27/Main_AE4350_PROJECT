183864fc7741f27e9bd3d9a77ababe67fdc78d11
kcoutinho27 <kfkcoutinho@gmail.com>
2022-08-30 14:51:49 +0200

    test

--- kc_test.py
+++ kc_test.py
@@ -1,12 +1,12 @@
-print("Hello World")
+#print("Hello World")
 import gym
 import tensorflow as tf
 import numpy as np
 from collections import deque
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense
+from tensorflow.keras.models import  Sequential, load_model, Model
+from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense, BatchNormalization, GlobalMaxPooling2D, MaxPooling2D
 from tensorflow.keras.optimizers import Adam
-
+import matplotlib.pyplot as plt
 from tensorflow import keras
 from collections import deque
 import time
@@ -25,33 +25,28 @@ import os
 from gym.envs.box2d.car_racing import CarRacing #change to car_racing_kc for custom car racing environment
 from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense
 
-
+#seed for repeatable results
 RANDOM_SEED = 5
-tf.random.set_seed(RANDOM_SEED)#seed for repeatable results
+tf.random.set_seed(RANDOM_SEED)
 
 env = CarRacing()
 #print("hello world")
 
-env.seed(RANDOM_SEED)#seed for repeatable results
+env.seed(RANDOM_SEED)
 np.random.seed(RANDOM_SEED)
 
-print(env.observation_space.shape)
+#choose number of episodes to train
+train_episodes = 50
+#test_episodes = 2
 
-# print("Action Space: {}".format(env.action_space))
-# print("State space: {}".format(env.observation_space))
-
-# An episode a full game
-train_episodes = 300
-test_episodes = 100
-
-
+#Build the DQN agent using sequential and 2d Concolution layers
 def agent(state_shape, action_shape): #Build model DQN
     """ The agent maps X-states to Y-actions
     e.g. The neural network output is [.1, .7, .1, .3]
     The highest value 0.7 is the Q-Value.
     The index of the highest action (0.7) is action #1.
     """
-    learning_rate = 0.01
+    learning_rate = 0.002
     model = Sequential()
     model.add(Conv2D(filters=6, kernel_size=(7, 7), strides=3, activation='relu', input_shape=state_shape))
     model.add(MaxPooling2D(pool_size=(2, 2)))
@@ -60,19 +55,7 @@ def agent(state_shape, action_shape): #Build model DQN
     model.add(Flatten())
     model.add(Dense(216, activation='relu'))
     model.add(Dense(action_shape, activation=None))
-    model.compile(loss='mean_squared_error', optimizer=Adam(lr=learning_rate, epsilon=1e-7))
-
-
-
-    # init = tf.keras.initializers.HeUniform()
-    # model = keras.Sequential()
-    # model.add(keras.layers.Dense(24, input_shape=state_shape, activation='relu', kernel_initializer=init))
-    # model.add(keras.layers.Dense(12, activation='relu', kernel_initializer=init))
-    # #model.add(keras.layers.Dense(action_shape, activation='linear', kernel_initializer=init))
-    # model.add(Dense(216, activation='relu', kernel_initializer=init))
-    # model.add(keras.layers.Dense(action_shape, activation='linear', kernel_initializer=init))
-    # model.compile(loss=tf.keras.losses.Huber(), optimizer=tf.keras.optimizers.Adam(lr=learning_rate),
-    #               metrics=['accuracy'])
+    model.compile(loss='mean_squared_error', optimizer=Adam(lr=learning_rate, epsilon=1e-7)) #optimizer
     return model
 
 #Function to get q values
@@ -81,14 +64,15 @@ def get_qs(model, state, step):
 
 #Training function
 def train(env, replay_memory, model, target_model, done):
-    learning_rate = 0.1  # Learning rate
+    learning_rate = 0.002  # Learning rate
     discount_factor = 0.618
 
     MIN_REPLAY_SIZE = 10000
     if len(replay_memory) < MIN_REPLAY_SIZE:
         return
 
-    batch_size = 34 * 2 #size on which the model trains
+    # size on which the model trains
+    batch_size = 44 * 2
     mini_batch = random.sample(replay_memory, batch_size)
     current_states = np.array([transition[0] for transition in mini_batch])
     current_qs_list = model.predict(current_states)
@@ -105,8 +89,8 @@ def train(env, replay_memory, model, target_model, done):
             max_future_q = reward
 
         current_qs = current_qs_list[index]
-        print("maxfutureq")
-        print(observation, action, reward, new_observation, done)
+        # print("maxfutureq")
+        # print(observation, action, reward, new_observation, done)
         #max_index = list(action).index(np.argmax(action))
         #print(max_index)
         current_qs[action] = (1 - learning_rate) * current_qs[action] + learning_rate * max_future_q
@@ -124,11 +108,13 @@ def main():
 
     # 1. Initialize the Target and Main models
     # Main Model (updated every 4 steps)
-    print("env action space")
-    print(env.action_space.n)
     model = agent(env.observation_space.shape, env.action_space.n)
+    #model.save("kc_model")
+    #model = tf.keras.models.load_model("kc_model_200_gfg")
+    #model = tf.keras.models.load_weights("kc_Model_weights_gfg")
     # Target Model (updated every 100 steps)
-    #    target_model = agent(env.observation_space.shape, env.action_space.n)
+    target_model = agent(env.observation_space.shape, env.action_space.n)
+    # target_model = tf.keras.models.load_model("kc_model_200_gfg")
     target_model = agent(env.observation_space.shape, env.action_space.n)
     target_model.set_weights(model.get_weights())
 
@@ -139,34 +125,35 @@ def main():
     # X = states, y = actions
     X = []
     y = []
-
+    rewards_tab = []
     steps_to_update_target_model = 0
-
+    elapsed_time = []
     for episode in range(train_episodes):
+        start_time = time.time()
         total_training_rewards = 0
         observation = env.reset()
         done = False
         while not done:
             steps_to_update_target_model += 1
-            if True:
-                env.render()
+            # if True: #remove render when training
+            #     env.render()
 
             random_number = np.random.rand()
             # 2. Explore using the Epsilon Greedy Exploration Strategy
             if random_number <= epsilon:
-                # Explore
+                # Use random action by sampling action space
                 action = env.action_space.sample()
             else:
                 # Exploit best known action
                 # model dims are (batch, env.observation_space.n)
                 encoded = observation
                 encoded_reshaped = encoded.reshape([1, encoded.shape[0], encoded.shape[1], encoded.shape[2]])
-                print("Re-shaped",encoded_reshaped.shape)
+                # print("Re-shaped",encoded_reshaped.shape)
                 predicted = model.predict(encoded_reshaped).flatten()
-                print("I am predicted: ", predicted)
+                # print("I am predicted: ", predicted)
                 action = np.argmax(predicted)
-                print("I am best action")
-                print(action)
+                # print("I am best action")
+                # print(action)
 
             #print(action)
             #print(len(env.step(action)))
@@ -175,28 +162,71 @@ def main():
             replay_memory.append([observation, action, reward, new_observation, done])
 
             # 3. Update the Main Network using the Bellman Equation
-            if steps_to_update_target_model % 4 == 0 or done:
+            if steps_to_update_target_model % 2 == 0 or done: #learn per x number of episodes
                 train(env, replay_memory, model, target_model, done)
+                #model.save("kc_model_100_p3")- doesnt really work
+
 
             observation = new_observation
             total_training_rewards += reward
-            #print(total_training_rewards)
-            if abs(total_training_rewards) > 125:
-                done = True
-            if done:
-                print('Total training rewards: {} after n steps = {} with final reward = {}'.format(
-                    total_training_rewards, episode, reward))
-                total_training_rewards += 1
 
-                if steps_to_update_target_model >= 100:
-                    print('Copying main network weights to the target network weights')
-                    target_model.set_weights(model.get_weights())
-                    steps_to_update_target_model = 0
-                break
+
+            end_time = time.time()
+            time_done = end_time - start_time
+            #stop episode at given time, time used is 3 min = 180 seconds
+            if time_done > 240:
+                done = True
+                if done:
+                    print(' Total training rewards: {} after n steps = {} with final reward = {}'.format(
+                        total_training_rewards, episode, reward))
+                    #rewards_tab.append(total_training_rewards)
+                    # model.save("kc_model_100_p2")-not worked
+                    total_training_rewards += 1
+                    rewards_tab.append(total_training_rewards)
+
+
+                    if steps_to_update_target_model >= 20:
+                        print('Copying main network weights to the target network weights')
+                        target_model.set_weights(model.get_weights())
+                        steps_to_update_target_model = 0
+                        break
 
         epsilon = min_epsilon + (max_epsilon - min_epsilon) * np.exp(-decay * episode)
+    episode_tab = np.arange(1, train_episodes+1, 1)
+    # print(rewards_tab, "space", np.array(rewards_tab))
+    # print(len(episode_tab), len(rewards_tab), len(np.array(rewards_tab)))
+    #model.save('kc_model_200_gfg')
+    #model.save_weights('Model_weights_gfg')
+    plt.plot(episode_tab, np.array(rewards_tab))
+    #plt.scatter(episode_tab, np.array(rewards_tab))
+    plt.xlabel('episode')
+    plt.ylabel('reward')
+    plt.title('Graph showing the reward obtained by agent for each episode')
+    plt.show()
+    plt.savefig('50_eps_time_1')
+
     env.close()
 
-
+#Run the code
 if __name__ == '__main__':
     main()
+
+
+#print(total_training_rewards)
+            # done = True
+            # if done:
+            #     print(' Total training rewards: {} after n steps = {} with final reward = {}'.format(
+            #          total_training_rewards, episode, reward))
+            #     #model.save("kc_model_100_p2")
+            #     total_training_rewards += 1
+            #
+            #     if steps_to_update_target_model >= 20:
+            #         print('Copying main network weights to the target network weights')
+            #         target_model.set_weights(model.get_weights())
+            #         steps_to_update_target_model = 0
+            #     end_time = time.time()
+            #     time_done = end_time - start_time
+            #     if time_done > 30:
+            #         elapsed_time.append(time_done)
+            #         print(time_done)
+            #         break
--- rl_testing.py
+++ rl_testing.py
@@ -1,54 +1,68 @@
 import gym
+import tensorflow as tf
+import numpy as np
+from collections import deque
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense
+from tensorflow.keras.optimizers import Adam
+
+from tensorflow import keras
+from collections import deque
+import time
+import random
+from tensorflow import keras
+#from DQN_agent_kc import agent, qvals, train_agent
+from collections import deque
 from stable_baselines3 import PPO
 from stable_baselines3.common.vec_env import DummyVecEnv
 from stable_baselines3.common.evaluation import evaluate_policy
 #################
-import time
 import torch
 import torchvision
 time.clock = time.time
 import os
+from gym.envs.box2d.car_racing_mike import CarRacing #change to car_racing_kc for custom car racing environment
+from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense
 
 #If you want to render in human mode, initialize the environment in this way: gym.make('EnvName', render_mode='human') and don't call the render method.
 
-environment_name = "CarRacing-v2"
-env = gym.make(environment_name)
+env = CarRacing()
 env.reset()
 new_step_api=True
-env.action_space.sample()
-env.observation_space
-
-#Test environment with random action, can't take turns
-episodes = 2
-for episode in range(1, episodes + 1):
-    state = env.reset()
-    done = False
-    score = 0
-
-    while not done:
-        env.render()
-        action = env.action_space.sample()
-        n_state, reward, done, info = env.step(action)
-        score += reward
-    print('Episode:{} Score:{}'.format(episode, score))
-env.close()
 
 # #################
 # #applying algorithm
 # env = gym.make(environment_name)
-# env = DummyVecEnv([lambda: env])#use dummy vec as wrapper for stacking frames
+env = DummyVecEnv([lambda: env])#use dummy vec as wrapper for stacking frames
 #
-# log_path = os.path.join('Training', 'Logs')
-# #model = PPO("CnnPolicy", env, verbose=1, tensorboard_log=log_path)#ppo algorithm used from stable baselines
+log_path = os.path.join('Training', 'Logs')
+model = PPO("CnnPolicy", env, verbose=1, tensorboard_log=log_path)#ppo algorithm used from stable baselines
+
+model.learn(total_timesteps=40000)#choose steps to train, otherwise car racing environment trains until 900 points
+#reward, this may take too long
+#del model
+ppo_path = os.path.join('Training', 'Saved Models', 'PPO_Driving_model50k')
+#del model
+
+model = PPO.load(ppo_path, env)
+model.save(ppo_path)
+
+evaluate_policy(model, env, n_eval_episodes=1, render=True)
+env.close()
+
+
+#Test environment with random action, can't take turns
+# episodes = 2
+# for episode in range(1, episodes + 1):
+#     state = env.reset()
+#     done = False
+#     score = 0
 #
-# #model.learn(total_timesteps=40000)#choose steps to train, otherwise car racing environment trains until 900 points
-# #reward, this may take too long
-# #del model
-# ppo_path = os.path.join('Training', 'Saved Models', 'PPO_Driving_model50k')
-# #del model
-#
-# model = PPO.load(ppo_path, env)
-# #model.save(ppo_path)
-#
-# #evaluate_policy(model, env, n_eval_episodes=1, render=True)
+#     while not done:
+#         env.render()
+#         action = env.action_space.sample()
+#         n_state, reward, done, info = env.step(action)
+#         score += reward
+#     print('Episode:{} Score:{}'.format(episode, score))
 # env.close()
+
